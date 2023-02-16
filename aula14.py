0e23e08e339128a1d33c843070b05cf499c26ebf
VismarF <vismarjc@hotmail.com>
2023-02-16 16:14:20 -0300

    'origin/master'

--- aula14.py
+++ aula14.py
@@ -1,1 +1,11 @@
 #função format
+a = 'A'
+b = 'B'
+c = 1.1
+string = 'b={nome2} \na={nome1} \na={nome1} \na={nome1} \nc={nome3:.2f}'
+
+formato = string.format(
+   nome1=a, nome2=b, nome3= c)
+
+
+print(formato)
--- teste2.py
+++ teste2.py
@@ -1,1 +1,5 @@
-print('explicito', 'é', 'melhor " do que implicito')
+numero_1=10
+numero_2=20
+resultado = numero_1 * numero_2
+
+print(resultado)
