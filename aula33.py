54c30505b2f817b3a8ef0b0d79d07bd2e6ca94b7
VismarF <vismarjc@hotmail.com>
2023-08-21 23:07:00 -0300

    Operadores de atribuição

---
+++ aula33.py
@@ -0,0 +1,15 @@
+"""
+Operadores de atribuição
+
+= += -= *= /= //= **= %=
++=(contador = contador + o novo valor)
+//(divisão inteira)
+**=(potenciação)
+%=(modulo)
+"""
+contador = 0
+while contador <= 10:
+    contador += 1
+    print(contador)
+
+print('Acabou')
