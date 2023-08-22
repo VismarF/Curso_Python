a405c22a4308fd62602d569d320b6b0267285105
VismarF <vismarjc@hotmail.com>
2023-08-21 22:39:55 -0300

    Repetições usando  while

---
+++ aula32.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while (enquanto)
+Executa uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador < 10: ##Enquanto o contador for menor que 10 ele ficara em loop
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
